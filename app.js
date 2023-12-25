4b3b889d6feef8e386c23f7cbe14cfbc53c76cf8
Eyob Mekonnen <mekoneneyob@5gmail.com>
2023-12-25 01:00:36 -0800

    more on security

--- app.js
+++ app.js
@@ -1,27 +1,64 @@
 const express = require('express');
 const morgan = require('morgan'); //logger middleware
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tours');
 const userRouter = require('./routes/users');
 const app = express();
-// 1.MiddleWare(order of middleware matters)
-console.log(process.env.NODE_ENV);
-app.use(express.json());
-app.use(express.static('public'));
-app.use((req, res, next) => {
-  console.log('hello middleware');
-  next();
+// 1.GLOBAL MiddleWare(order of middleware matters)
+
+//SET SECURE HTTP HEADERS
+app.use(helmet());
+
+// DEVELOPMENT LOGGING
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// LIMIT REQUESET FROM SAME API
+const limiter = rateLimit({
+  max: 3,
+  windowMs: 60 * 60 * 1000,
+  message: 'too many request from this Ip,please try again in an a hour'
 });
+app.use('/api', limiter);
+
+//BODY PARSER,READ DATA FROM BODY INTO REQ.BODY
+app.use(express.json({ limit: '10kb' }));
+
+// DATA SANITIZATION AGAINST NO SQL QUERY INJECTION
+app.use(mongoSanitize());
+
+//DATA SANITIZATION AGAINST XSS(FROM INJECTING SCRIPTS TO BODY DATA)
+app.use(xss());
+
+//PREVENT PARAMETER POLLUTION
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'difficulty',
+      'price',
+      'maxGroupSize'
+    ]
+  })
+);
+// SERVE STATIC FILES
+app.use(express.static('public'));
+
+//TEST MIDDLEWARE
 app.use((req, res, next) => {
   req.requetedTime = new Date().toISOString();
   // console.log(x);
   // console.log(req.header);
   next();
 });
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
